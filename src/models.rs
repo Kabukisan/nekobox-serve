289bfe87fc075b6cac787b912617b98d4dcc25d2
Kabukisan <122027212+Kabukisan@users.noreply.github.com>
2023-01-08 18:56:40 +0100

    Add request and response structs for rest api

---
+++ src/models.rs
@@ -0,0 +1,109 @@
+use chrono::{DateTime, Utc};
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+pub enum MediaType {
+    Video,
+    Audio,
+}
+
+#[derive(Serialize, Deserialize)]
+pub enum MediaFormat {
+    Aac,
+    Flv,
+    M4a,
+    Mp3,
+    Mp4,
+    Ogg,
+    Wav,
+    Webm,
+}
+
+#[derive(Deserialize)]
+pub struct TaskCreateRequest {
+    url: String,
+    media_type: MediaType,
+    format: MediaFormat,
+}
+
+#[derive(Serialize, Deserialize)]
+pub enum TaskStatus {
+    Pending,
+    Complete,
+    Error,
+}
+
+
+#[derive(Deserialize)]
+pub struct TaskRequest {
+    status_id: String,
+}
+
+impl TaskRequest {
+    pub fn new<S: Into<String>>(status_id: S) -> Self {
+        TaskRequest {
+            status_id: status_id.into(),
+        }
+    }
+}
+
+#[derive(Serialize)]
+pub struct TaskResponse {
+    status_id: String,
+    status: TaskStatus,
+    message: Option<String>,
+    percentage: f32,
+}
+
+impl TaskResponse {
+    pub fn new<S: Into<String>>(status_id: S, status: TaskStatus, message: Option<String>, percentage: f32) -> Self {
+        TaskResponse {
+            status_id: status_id.into(),
+            status,
+            message,
+            percentage,
+        }
+    }
+
+    pub fn pending<S: Into<String>>(status_id: S) -> Self {
+        Self::new(status_id, TaskStatus::Pending, None, 0.0)
+    }
+
+    pub fn complete<S: Into<String>, M: Into<Option<String>>>(status_id: S, message: M) -> Self {
+        Self::new(status_id, TaskStatus::Complete, message.into(), 1.0)
+    }
+
+    pub fn error<S: Into<String>, M: Into<Option<String>>>(status_id: S, message: M) -> Self {
+        Self::new(status_id, TaskStatus::Error, message.into(), 0.0)
+    }
+}
+
+#[derive(Deserialize)]
+pub struct AuthLoginRequest {
+    username: String,
+    password: String,
+}
+
+impl AuthLoginRequest {
+    pub fn new<S: Into<String>>(username: S, password: S) -> Self {
+        AuthLoginRequest {
+            username: username.into(),
+            password: password.into(),
+        }
+    }
+}
+
+#[derive(Serialize)]
+pub struct AuthLoginResponse {
+    token: String,
+    expires: DateTime<Utc>,
+}
+
+impl AuthLoginResponse {
+    pub fn new<S: Into<String>>(token: S, expires: DateTime<Utc>) -> Self {
+        AuthLoginResponse {
+            token: token.into(),
+            expires,
+        }
+    }
+}
